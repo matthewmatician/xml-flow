3db9184bb90c1e03d27a7c786ebe47aedd5bb9c4
Diego Pérez <ezequiel@neutrona.com>
2016-04-19 11:45:54 -0300

    added 'tag:<<ns>> <<tag>>

--- test/xml-flow.js
+++ test/xml-flow.js
@@ -52,6 +52,19 @@ describe('xml-flow', function() {
       });
     });
 
+    it('should listen to tags with namespaces', function(done) {
+      var simpleStream = getFlow('./test/with-ns.xml')
+      , output = {
+        $name: 'alien',
+        $ns: 'my:ns'
+      }
+      ;
+
+      simpleStream.on('tag:my:ns alien', function() {
+        done();
+      });
+    });
+
     it('should make non-attributed data look really simple', function(done) {
       var simpleStream = getFlow('./test/test.xml')
         , output = {
